fd796d74fdbd4002f29b0f9ce8186878b4213d15
DEREK KENDRICK <derekkednrick79@gmail.com>
2022-07-08 21:56:57 -0400

    frontend

---
+++ src/components/add-book.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function addBook() {
+  const [name, setName] = useState("");
+  const [author, setAuthor] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescription] = useState("");
+
+  const postBook = (event) => {
+    axios
+      .post("http://127.0.0.1:5000/book/add", {
+        name: name,
+        author: author,
+        price: price,
+        description: description,
+      })
+      .then((response) => console.log(response))
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div className="add-book">
+      <h1 className="add-book-title">Enter your books data!</h1>
+      <form className="add-book-form" onSubmit={postBook}>
+        <input
+          className="add-book-form-input"
+          onChange={(event) => setName(event.target.value)}
+          type="text"
+          placeholder="name"
+        />
+        <input
+          className="add-book-form-input"
+          onChange={(event) => setAuthor(event.target.value)}
+          type="text"
+          placeholder="author"
+        />
+        <input
+          className="add-book-form-input"
+          onChange={(event) => setPrice(event.target.value)}
+          type="number"
+          placeholder="price"
+        />
+        <textarea
+          className="add-book-form-input"
+          onChange={(event) => setDescription(event.target.value)}
+          type="text"
+          placeholder="description"
+        />
+        <button className="add-book-form-input">Add book!</button>
+      </form>
+    </div>
+  );
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <h1>Hello welcome to my book collection!!</h1>
+      <div className="navbar-links">
+        <NavLink
+          exact
+          className="navbar-link"
+          activeClassName="selected"
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className="navbar-link"
+          activeClassName="selected"
+          to="/add-book"
+        >
+          Add a Book!
+        </NavLink>
+      </div>
+    </div>
+  );
+}
